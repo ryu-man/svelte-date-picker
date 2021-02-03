a3bc9642cadb4d8fb27cb0b3575fb5dcf9843c99
ryu-man <rioman19@gmail.com>
2021-02-03 19:52:07 +0100

    style: no code change

--- src/picker/date_input.svelte
+++ src/picker/date_input.svelte
@@ -1,23 +1,23 @@
 <script>
-	import { format as dateFormat } from "date-fns";
-	export let format
-	export let date
+  import { format as dateFormat } from 'date-fns'
+  export let format
+  export let date
 
-	function formatter(date,format){
-		try {
-			return dateFormat(date, format)
-		} catch (error) {
-			return format
-		}
-	}
+  function formatter(date, format) {
+    try {
+      return dateFormat(date, format)
+    } catch (error) {
+      return format
+    }
+  }
 </script>
 
-<style>
-	.date-input{
-		background-color: rgb(0 0 0 / 3%);
-		border-radius: 1px;
-		padding: 3px;
-	}
-</style>
+<span class="date-input"> {formatter(date, format)} </span>
 
-<span class="date-input"> {formatter(date,format)} </span>
+<style>
+  .date-input {
+    background-color: rgb(0 0 0 / 3%);
+    border-radius: 1px;
+    padding: 3px;
+  }
+</style>
